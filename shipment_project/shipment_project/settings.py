601aa5a49e732576f1f4948f210fe84729023e4d
sunil <sunilbaludadi@gmail.com>
2025-06-03 13:16:31 +0000

    settings.py

--- shipment_project/shipment_project/settings.py
+++ shipment_project/shipment_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2bs68hq6l1!u=xo2ye*w@85w-oxljggodp-n)v%d4k(5*8f39u'
-
+#SECRET_KEY = 'django-insecure-2bs68hq6l1!u=xo2ye*w@85w-oxljggodp-n)v%d4k(5*8f39u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Add localhost and 127.0.0.1 to ALLOWED_HOSTS
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','13.233.20.88','172.31.3.3','chaitanyalogistics.com','www.chaitanyalogistics.com']
 
 
 # Application definition
