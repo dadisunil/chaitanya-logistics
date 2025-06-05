90ce78f54de221ad7dbf5db364b233236da538ec
sunil <sunilbaludadi@gmail.com>
2025-06-05 21:20:07 +0000

    vite url to config.js

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+// config.ts
+// This file exports the API base URL. Since Vite does not load .env from src/, you must manually keep this in sync with your .env file.
+export const API_BASE_URL = "http://13.233.20.88:8000";
